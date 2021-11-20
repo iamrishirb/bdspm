f58435dfdff8b36eb29cc49b501ad832808cde16
iamrishirb <rishirbbajpai@outlook.com>
2021-11-20 12:25:52 +0530

    added initial markup

---
+++ header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>DigitalEdu an Education Category Bootstrap Responsive Website Template - Home : W3Layouts</title>
+    <!--/google-fonts-->
+    <link href="//fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;0,600;0,700;1,400&display=swap"
+        rel="stylesheet">
+    <!--//google-fonts-->
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="assets/css/style-starter.css">
+
+</head>
+
+<body>
